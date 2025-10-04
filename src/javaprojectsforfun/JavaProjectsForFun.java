faee3391d99802fa80579af14ec5d60794dac707
SBELUCCI <sofiabelucci08@gmail.com>
2025-10-04 03:36:16 -0300

    adding validations

--- src/javaprojectsforfun/JavaProjectsForFun.java
+++ src/javaprojectsforfun/JavaProjectsForFun.java
@@ -17,14 +17,22 @@ public class JavaProjectsForFun {
     public static void main(String[] args) {
         // TODO code application logic here
          Scanner scanner = new Scanner(System.in);
+        int number;
 
-        System.out.print("Ingresá un número: ");
+        while (true) {
+            System.out.print("Ingresa un numero entero: ");
 
-        // Read number (int)
-        int number = scanner.nextInt();
+            if (scanner.hasNextInt()) {
+                // Read number (int)
+                number = scanner.nextInt();
+                break;  // If the entered number is valid, exit the loop
+            } else {
+                System.out.println("Error: eso no es un numero entero valido. Intenta de nuevo.");
+                scanner.next(); // descarta la entrada inválida
+            }
+        }
 
-        System.out.println("Ingresaste el número: " + number);
-
+        System.out.println("Ingresaste el numero: " + number);
         scanner.close();
     }
     
